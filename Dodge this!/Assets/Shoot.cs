5763b8fa9e15d351298088373237e72077d3e049
pijuskri <pijuskri@gmail.com>
2017-11-24 18:35:45 +0200

    added shooting(wip)

---
+++ Dodge this!/Assets/Shoot.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Shoot : MonoBehaviour
+{
+
+    public Rigidbody projectile;
+    public Transform bulletSpawn;
+    public float projectileForce = 500f;
+    public float fireRate = .25f;
+
+    private float nextFireTime;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire2") && Time.time > nextFireTime)
+        {
+            Rigidbody cloneRb = Instantiate(projectile, bulletSpawn.position, Quaternion.identity) as Rigidbody;
+            cloneRb.AddForce(bulletSpawn.transform.forward * projectileForce);
+            nextFireTime = Time.time + fireRate;
+        }
+    }
+}
