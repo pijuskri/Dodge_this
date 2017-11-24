034dc92643cd85c1c4034e424172c6c14735037f
pijuskri <pijuskri@gmail.com>
2017-11-24 18:19:34 +0200

    Made basic character

---
+++ Dodge this!/Assets/Player.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+    CharacterController controller;
+    private float speed = 6.0f;
+    private float turnSpeed = 200.0f;
+    private Vector3 moveDirection = Vector3.zero;
+    private float gravity = 20.0f;
+
+    // Use this for initialization
+    void Start () {
+        controller = gameObject.GetComponent<CharacterController>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (controller.isGrounded)
+        {
+            moveDirection = transform.forward * Input.GetAxis("Vertical") * speed;
+        }
+
+        float turn = Input.GetAxis("Horizontal");
+        transform.Rotate(0, turn * turnSpeed * Time.deltaTime, 0);
+        controller.Move(moveDirection * Time.deltaTime);
+        moveDirection.y -= gravity * Time.deltaTime;
+    }
+}
