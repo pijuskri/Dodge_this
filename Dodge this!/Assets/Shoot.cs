4f50c2fba02cb863a0a0fd6fddf2ec4b61266a15
pijuskri <pijuskri@gmail.com>
2017-11-25 06:01:26 +0200

    added smg

--- Dodge this!/Assets/Shoot.cs
+++ Dodge this!/Assets/Shoot.cs
@@ -7,6 +7,7 @@ public class Gun
     public int bullets;
     public double damage;
     public float fireRate;
+    public string fireMode;
     public GameObject gunObject;
     public GameObject bullet;
     public Gun() { }
@@ -35,7 +36,8 @@ public class Shoot : MonoBehaviour
         guns[0].name = "pistol";
         guns[0].bullets = 1;
         guns[0].damage = 1;
-        guns[0].fireRate = 0.25f;
+        guns[0].fireRate = 0.35f;
+        guns[0].fireMode = "semi";
         guns[0].gunObject = gunObjects[0];
         guns[0].bullet = bulletObjects[0];
 
@@ -45,8 +47,18 @@ public class Shoot : MonoBehaviour
         guns[1].bullets = 7;
         guns[1].damage = 1;
         guns[1].fireRate = 1f;
+        guns[1].fireMode = "semi";
         guns[1].gunObject = gunObjects[1];
         guns[1].bullet = bulletObjects[1];
+
+        guns[2] = new Gun();
+        guns[2].name = "smg";
+        guns[2].bullets = 1;
+        guns[2].damage = 1;
+        guns[2].fireRate = 0.1f;
+        guns[2].fireMode = "auto";
+        guns[2].gunObject = gunObjects[2];
+        guns[2].bullet = bulletObjects[0];
     }
     // Update is called once per frame
     void Update()
@@ -55,8 +67,7 @@ public class Shoot : MonoBehaviour
         if (Input.GetKeyUp(KeyCode.Keypad2)) gun = 1;
         fireRate = guns[gun].fireRate;
 
-        Transform bulletSpawn = guns[gun].gunObject.transform;
-        Rigidbody projectile = guns[gun].bullet.GetComponent<Rigidbody>();
+       
 
         for (int i = 0; i < guns.Length-1; i++)
         {
@@ -64,29 +75,46 @@ public class Shoot : MonoBehaviour
             guns[gun].gunObject.SetActive(true);
         }
 
-        if (Input.GetButtonDown("Fire2") && Time.time > nextFireTime)
+        if (guns[gun].fireMode == "semi")
         {
-            audioSource.PlayOneShot(gunSounds[gun]);
-            ParticleSystem muzzleFlash = guns[gun].gunObject.GetComponentInChildren<ParticleSystem>();
-            muzzleFlash.Play();
-            //Debug.Log(gun);
-            if (guns[gun].bullets == 1)
+            if (Input.GetButtonDown("Fire2") && Time.time > nextFireTime)
             {
-                Rigidbody cloneRb = Instantiate(projectile, bulletSpawn.position, bulletSpawn.rotation) as Rigidbody;
-                cloneRb.AddForce(bulletSpawn.transform.forward * projectileForce);
+                shoot();
             }
-            else
-            {
-                for (int i = 0; i < guns[gun].bullets; i++)
-                {
-                    Quaternion rand;
-                    rand = Quaternion.Euler( Random.Range(-10,10), Random.Range(-10, 10), Random.Range(-10, 10));
-                    //rand = Random.rotation;
-                    Rigidbody cloneRb = Instantiate(projectile, bulletSpawn.position, bulletSpawn.rotation * rand) as Rigidbody;
-                    cloneRb.AddForce(cloneRb.transform.forward * projectileForce);
-                }
-            }
-            nextFireTime = Time.time + fireRate;
         }
+        if (guns[gun].fireMode == "auto")
+        {
+            if (Input.GetButton("Fire2") && Time.time > nextFireTime)
+            {
+                shoot();
+            }
+        }
+    }
+    void shoot()
+    {
+        Transform bulletSpawn = guns[gun].gunObject.transform;
+        Rigidbody projectile = guns[gun].bullet.GetComponent<Rigidbody>();
+
+        audioSource.PlayOneShot(gunSounds[gun]);
+        ParticleSystem muzzleFlash = guns[gun].gunObject.GetComponentInChildren<ParticleSystem>();
+        muzzleFlash.Play();
+        //Debug.Log(gun);
+        if (guns[gun].bullets == 1)
+        {
+            Rigidbody cloneRb = Instantiate(projectile, bulletSpawn.position, bulletSpawn.rotation) as Rigidbody;
+            cloneRb.AddForce(bulletSpawn.transform.forward * projectileForce);
+        }
+        else
+        {
+            for (int i = 0; i < guns[gun].bullets; i++)
+            {
+                Quaternion rand;
+                rand = Quaternion.Euler(Random.Range(-10, 10), Random.Range(-10, 10), Random.Range(-10, 10));
+                //rand = Random.rotation;
+                Rigidbody cloneRb = Instantiate(projectile, bulletSpawn.position, bulletSpawn.rotation * rand) as Rigidbody;
+                cloneRb.AddForce(cloneRb.transform.forward * projectileForce);
+            }
+        }
+        nextFireTime = Time.time + fireRate;
     }
 }
